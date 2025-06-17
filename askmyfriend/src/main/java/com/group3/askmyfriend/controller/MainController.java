8ce8251cbf41f1e7123432986600516064d2c2aa
박준형 <junhyung0125@gmail.com>
2025-06-17 23:46:02 +0900

    MainController 추가 및 index 리다이렉트 설정

---
+++ askmyfriend/src/main/java/com/group3/askmyfriend/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.group3.askmyfriend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    // localhost:8080/index로 접속 시 index.html 보여줌
+    @GetMapping("/index")
+    public String index() {
+        return "index"; // templates/index.html
+    }
+
+    // localhost:8080 접속 시에도 /index로 리다이렉트
+    @GetMapping("/")
+    public String homeRedirect() {
+        return "redirect:/index";
+    }
+}
